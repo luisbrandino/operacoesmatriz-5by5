8c150b027ddc22a74da40a9e31e265603b5ba933
luisbrandino <luisbrandino.contato@gmail.com>
2024-04-24 21:32:32 -0300

    feat: added a function to select the desired operator and calculate the values, given said operator

--- operacoesmatriz/Program.cs
+++ operacoesmatriz/Program.cs
@@ -4,7 +4,6 @@ int columns = 3;
 double[,] matrixA = new double[rows, columns];
 double[,] matrixB = new double[rows, columns];
 double[,] result = new double[rows, columns];
-
 void randomizeMatrix(double[,] matrix, int min = 0, int max = 10)
 {
     for (int i = 0; i < rows; i++)
@@ -23,6 +22,45 @@ void printMatrix(double[,] matrix)
     }
 }
 
+double calculate(char operation, double a, double b)
+{
+    switch (operation)
+    {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        default:
+            if (b == 0)
+                return double.NaN;
+            return a / b;
+    }
+}
+char selectOperatorMenu()
+{
+    Console.Write("Informe a operação que deseja executar nas matrizes (+, -, *, /): ");
+    char operation = Console.ReadLine().First();
+
+    while (true)
+    {
+        bool correctOperator =
+            operation == '+' ||
+            operation == '-' ||
+            operation == '*' ||
+            operation == '/';
+
+        if (correctOperator)
+            break;
+
+        Console.Write("Operador inválido, tente novamente (+, -, *, /): ");
+        operation = Console.ReadLine().First();
+    }
+
+    return operation;
+}
+
 randomizeMatrix(matrixA);
 randomizeMatrix(matrixB);
 
@@ -34,42 +72,14 @@ Console.WriteLine("\nMatriz B:\n");
 
 printMatrix(matrixB);
 
-for (int i = 0; i < rows; i++)
-    for (int j = 0; j < columns; j++)
-        result[i, j] = matrixA[i, j] + matrixB[i, j];
+Console.WriteLine();
 
-Console.WriteLine("\nSoma:\n");
+char operation = selectOperatorMenu();
 
-printMatrix(result);
+Console.WriteLine($"\nResultado ({operation}):");
 
 for (int i = 0; i < rows; i++)
-    for (int j = 0; j < columns; j++)
-        result[i, j] = matrixA[i, j] - matrixB[i, j];
-
-Console.WriteLine("\nSubtração:\n");
-
-printMatrix(result);
-
-for (int i = 0; i < rows; i++)
-    for (int j = 0; j < columns; j++)
-        result[i, j] = matrixA[i, j] * matrixB[i, j];
-
-Console.WriteLine("\nMultiplicação:\n");
-
-printMatrix(result);
-
-for (int i = 0; i < rows; i++)
-    for (int j = 0; j < columns; j++)
-    {
-        if (matrixB[i, j] == 0)
-        {
-            result[i, j] = double.NaN;
-            continue;
-        }
-
-        result[i, j] = matrixA[i, j] / matrixB[i, j];
-    }
-
-Console.WriteLine("\nDivisão:\n");
+    for (int j = 0; j < rows; j++)
+        result[i, j] = calculate(operation, matrixA[i, j], matrixB[i, j]);
 
 printMatrix(result);
